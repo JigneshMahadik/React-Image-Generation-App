6e158cdd2452c0d46ca8a94ebdedcd92a67a29f6
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-03-21 23:50:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cont">
+        <h2>Image Generation App</h2>
+        <input type="text" placeholder="Enter Text..."/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
